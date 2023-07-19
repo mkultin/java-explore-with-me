ab81f63a8c7ae2ca98f2e3c34d8866ebf58046ff
mkultin <mkultin@yandex.ru>
2023-07-20 00:36:17 +0300

    fix: stat-service/exceptions

--- stat-service/service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ stat-service/service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.practicum.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,6 +28,13 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException exception) {
+        log.warn(exception.getMessage(), exception);
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissServletReqParamExp(final MissingServletRequestParameterException exception) {
         log.warn(exception.getMessage(), exception);
         return new ErrorResponse(exception.getMessage());
     }
